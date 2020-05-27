a7174592669d4c930c551d5affa0c60301750bcc
MRizzo <mrizzo25@gmail.com>
2020-05-27 19:39:52 -0300

    add activity menu

---
+++ app/src/main/java/com/mrizzo/bigdata/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.mrizzo.bigdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
